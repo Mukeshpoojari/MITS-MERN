ae27e8436e354e0c8faa438c5288d120d4277b46
Mukeshpoojari <pujarimukesh31@gmail.com>
2026-01-22 15:56:49 +0530

    js basic 2nd day

---
+++ script.js
@@ -0,0 +1,62 @@
+console.log("hello world");
+var a;
+a = 10;
+console.log(a);
+let b = 20;
+let c = b + a;
+
+console.log(b);
+console.log(c);
+
+
+var a=30;
+var str="hello";
+var bool=true;
+var und;
+var big=BigInt(12345678901234567890);
+var sym=Symbol("id");
+console.log(typeof a);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof und);
+console.log(typeof big);
+console.log(typeof sym);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**2);
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a==b);
+console.log(a!=b);
+
+a=true;
+b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);     
+
+var arr=[1,2,3,4,5];
+console.log(arr);
+
+let age = 20;
+
+let result = (age >= 18) ? "Adult" : "Minor";
+console.log(result);
+
+var arr1=[10,20,30];
+var arr2=[...arr1,40,50,60];
+console.log(arr2);
+
+var [m1,m2,m3,m4 ,m5]=[80,90,89,98,77];
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
